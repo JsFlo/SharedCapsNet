3d3d3e7da87c5a7931b4d388708897b1f9779714
Jose Flores <jsflores480@gmail.com>
2018-01-07 17:50:39 -0600

    Adds a middle caps layer

--- Model.py
+++ Model.py
@@ -18,28 +18,30 @@ class Model(object):
         conv_caps_layer = ConvCapsuleLayer(1152, 8)
         conv_caps = conv_caps_layer(input_image_batch)
 
-        digit_caps_layer = CapsuleLayer(1152, 8, 10, 16, ConvAdapter())
-        routing_output = digit_caps_layer(conv_caps, batch_size)
+        digit_caps_layer = CapsuleLayer(1152, 8, 100, 16, ConvAdapter())
+        routing_output1 = digit_caps_layer(conv_caps, batch_size)
         # (?, 1, caps, dims, 1)
 
+        digit_caps2_layer = CapsuleLayer(100, 16, 10, 16, CapsAdapter())
+        routing_output2 = digit_caps2_layer(routing_output1, batch_size)
         # test_digit_caps_layer = CapsuleLayer(10, 16, 5, 3, CapsAdapter())
         # test_routing_output = test_digit_caps_layer(routing_output, batch_size)
 
         # single digit prediction
-        single_digit_prediction = self._transform_model_output_to_a_single_digit(routing_output)
+        single_digit_prediction = self._transform_model_output_to_a_single_digit(routing_output2)
         # (?, )
 
         # labels
         correct_labels_placeholder = tf.placeholder(shape=[None], dtype=tf.int64)
 
         # loss
-        margin_loss = get_margin_loss(correct_labels_placeholder, routing_output)
+        margin_loss = get_margin_loss(correct_labels_placeholder, routing_output2)
         mask_with_labels = tf.placeholder_with_default(False, shape=())
 
         reconstruction_loss, decoder_output, masked_out = _get_reconstruction_loss(mask_with_labels,
                                                                                    correct_labels_placeholder,
                                                                                    single_digit_prediction,
-                                                                                   routing_output,
+                                                                                   routing_output2,
                                                                                    input_image_batch)
 
         # keep it small
@@ -52,7 +54,7 @@ class Model(object):
         optimizer = tf.train.AdamOptimizer()
         self.training_op = optimizer.minimize(final_loss)
 
-        self.digit_caps_routing_output = routing_output
+        self.digit_caps_routing_output = routing_output2
         self.final_loss = final_loss
         self.correct = correct
         self.accuracy = accuracy
